86c016d2594dea1342d54be2b397e14bf223b28b
siddharth parashar <20308880+shanu516516@users.noreply.github.com>
2024-02-14 15:02:58 +0530

    relayer test for api begin

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
-
+use r2d2_postgres::postgres::NoTls;
+use r2d2_postgres::PostgresConnectionManager;
 lazy_static! {
     pub static ref RELAYER_VERSION: String =
         std::env::var("RelayerVersion").expect("missing environment variable RelayerVersion");
@@ -14,4 +15,17 @@ lazy_static! {
         .expect("missing environment variable RPC_SERVER_THREAD")
         .parse::<usize>()
         .unwrap();
+
+        pub static ref POSTGRESQL_POOL_CONNECTION: r2d2::Pool<PostgresConnectionManager<NoTls>> = {
+            dotenv::dotenv().expect("Failed loading dotenv");
+            // POSTGRESQL_URL
+            let postgresql_url =
+                std::env::var("POSTGRESQL_URL").expect("missing environment variable POSTGRESQL_URL");
+            let manager = PostgresConnectionManager::new(
+                // TODO: PLEASE MAKE SURE NOT TO USE HARD CODED CREDENTIALS!!!
+                postgresql_url.parse().unwrap(),
+                NoTls,
+            );
+            r2d2::Pool::new(manager).unwrap()
+        };
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,17 @@ pub mod relayer;
 
 #[macro_use]
 extern crate lazy_static;
+
+#[cfg(test)]
+mod test {
+
+    use relayerarchiverlib::{database::TraderOrder as TraderOrderDB, RelayerDB};
+    #[test]
+    fn test_check_get_order_by_uuid_from_archiver() {
+        dotenv::dotenv().expect("Failed loading dotenv");
+        let database_url = std::env::var("DATABASE_URL").expect("No database url found!");
+        let relayer_db = RelayerDB::from_host(database_url);
+        let mut pool = relayer_db.get_conn().unwrap();
+        TraderOrderDB::get_by_uuid(&mut *pool, "".to_string());
+    }
+}
